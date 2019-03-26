b094a7a577f2972a69aa3aae65bdfcc7a1f683b6
Dennis <denis.kompaniiets@gmail.com>
2019-03-26 10:52:38 +0200

    techspec for magic methods

---
+++ magic.py
@@ -0,0 +1,23 @@
+import time
+
+
+class timer:
+
+    def __init__(self):
+        self.start = time.time()
+
+    @property
+    def current_time(self):
+        return time.time() - self.start
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        return print('Elapsed: {}'.format(self.current_time))
+
+
+with timer() as t:
+    time.sleep(1)
+    print(t.current_time)
+    time.sleep(1)
